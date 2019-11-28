a609d2e8f2b3c8720de4e578a5f2b6cfd3929de4
Siddharth Bhat <siddu.druid@gmail.com>
2019-11-29 02:24:44 +0530

    add C++ file to parse sentence

---
+++ makefile
@@ -0,0 +1,2 @@
+rnn: rnn.cpp
+	nvcc -g rnn.cpp -o rnn -std=c++11
---
+++ rnn.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <set>
+#include <map>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <stdio.h>
+#include <assert.h>
+using namespace std;
+
+int last_word_index = 0;
+set<string> vocab;
+map<string, int> word2ix;
+map<int, string> ix2word;
+
+vector<vector<int> > sentences;
+
+void add_word_to_vocab(string w) {
+    if (vocab.find(w) != vocab.end()) { return; }
+    word2ix[w] = last_word_index;
+    ix2word[last_word_index] = w;
+    last_word_index++;
+}
+
+bool is_char_word_break(char c) {
+    return c == ' ' || c== '\t' || c == '\n' || c == '.';
+}
+
+// consume whitespace and get the next word, or empty string if run out.
+string parse_word(FILE *f) {
+    char c;
+    do {
+        c = fgetc(f);
+    } while(c != EOF && is_char_word_break(c));
+
+    if (c == EOF) { return ""; }
+    ungetc(c, f);
+
+    string w;
+    while(1) {
+        c = fgetc(f); 
+        if (is_char_word_break(c)) break;
+        w += c;
+    } 
+
+    return w;
+}
+
+bool consume_till_sentence_end_or_word(FILE *f) {
+    while(1) {
+        char c = fgetc(f);
+        if (c == '.') { return true; }
+        if (c == ' ' || c == '\t' || c == '\n')  { continue; }
+        ungetc(c, f);
+        return false;
+
+    }
+}
+
+vector<int> parse_sentence(FILE *f) {
+    vector<int> sentence;
+    while(1) {
+        // returns true if '.' was consumed
+        if (consume_till_sentence_end_or_word(f)) { return sentence; }
+        string w = parse_word(f);
+        if (w == "") { return sentence; }
+        auto it = word2ix.find(w);
+        assert(it != word2ix.end());
+        sentence.push_back(it->second);
+    }
+}
+
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        cout << "usage: <input-path>\n";
+        return 1;
+    }
+    FILE *f = fopen(argv[1], "r");
+
+    // construct vocab
+    while(1) {
+        string w = parse_word(f);
+        cout << "found word: " << w << "\n";
+        if (feof(f)) { break; }
+        add_word_to_vocab(w);
+    }
+    rewind(f);
+    while(1) {
+        vector<int> sentence = parse_sentence(f);
+        sentences.push_back(sentence);
+        if (feof(f)) { break; }
+    }
+    fclose(f);
+
+    for(auto it: word2ix) {
+        cout << "|" << it.first << "|->" << it.second << "\n";
+    }
+
+    cout << "SENTENCES:\n";
+    for(auto s: sentences) {
+        for (auto w : s) {
+            cout << w << " ";
+        }
+        cout << "\n";
+    }
+}
